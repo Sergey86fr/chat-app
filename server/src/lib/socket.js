1cb9369cf36bb01912caad3d65ac19a1eea83221
Sergey86fr <sergsfree@gmail.com>
2026-06-24 18:41:56 +0500

    add to ws

--- server/src/lib/socket.js
+++ server/src/lib/socket.js
@@ -10,6 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [ENV.CLIENT_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     },
 });
